7475335bcb7329fa136a962761e2d8dcbec3c628
wyliebenjamin2019 <42677949+wyliebenjamin2019@users.noreply.github.com>
2018-09-26 08:36:28 -0700

    Update MAIN.java

--- MAIN.java
+++ MAIN.java
@@ -16,8 +16,12 @@ public class Main {
         double d1=Math.sqrt(Math.pow(y1-y2,2)+Math.pow(x1-x2,2)+Math.pow(z1-z2,2));
         double d2=Math.sqrt(Math.pow(y2-y3,2)+Math.pow(x2-x3,2)+Math.pow(z2-z3,2));
         double d3=Math.sqrt(Math.pow(y3-y1,2)+Math.pow(x3-x1,2)+Math.pow(z3-z1,2));
-        System.out.println(d1);
-        System.out.println(d2);
-        System.out.println(d3);
+        double da = Math.max(d1,d2);
+        double db = Math.max(da,d3);
+        double dc = Math.min(d1,d2);
+        double dd = Math.min(dc,d3);
+        System.out.printf("Max distance ~%.2f",db);
+        System.out.printf("\nMin distance ~%.2f",dd);
+
     }
 }
